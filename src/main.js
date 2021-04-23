e67cb7911aee4e557e490aaa92c05b321d525048
于爽 <2659493979@qq.com>
2021-04-23 21:00:19 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/login'
+Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
 
