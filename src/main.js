705dd1c0631edee11a84e1d2c4f3110d5ef565a4
于爽 <2659493979@qq.com>
2021-04-27 17:43:45 +0800

    完成用户列表的功能

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 // 配置请求根路径
-axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/login'
+axios.defaults.baseURL='http://www.ysqorz.top:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  console.log(config)
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+//在最后必须都return
+  return config
+})
 Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
